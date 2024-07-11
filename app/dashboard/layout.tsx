51e5ab083b5a584cf2f0e7c55447f028624f2c6f
vijitha epa <vepa@bglcorp.com.au>
2024-07-12 09:51:04 +1000

    data streaming

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import React from "react";
+import Loading from "@/app/dashboard/(overview)/loading";
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
@@ -7,6 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
+
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
     );
