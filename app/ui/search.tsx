c8d8ee4e416257eb3d9405b94b26d1995d421d9e
vijitha epa <vepa@bglcorp.com.au>
2024-07-12 12:11:12 +1000

    introducing debounce ... delay on keystroke

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {useDebouncedCallback} from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -9,7 +10,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const pathname = usePathname();
     const { replace } = useRouter();
 
-    const handleChange = (input: string) => {
+    const handleChange = useDebouncedCallback((input: string) => {
         console.log(input);
         const params = new URLSearchParams(searchParam);
         if(input) {
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
             params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
-    }
+    }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
